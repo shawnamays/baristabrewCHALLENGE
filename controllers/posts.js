e58d68ff474605f21401d9b2a001a1ce5352b122
Shawna Mays <shawnadmays@gmail.com>
2022-11-22 21:57:48 -0500

    working ont heproject

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,6 @@
-const cloudinary = require("../middleware/cloudinary");
+// const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
-const Comment = require("../models/Comment");
+// const Comment = require("../models/Comment");
 
 
 module.exports = {
@@ -31,19 +31,13 @@ module.exports = {
   },
   createPost: async (req, res) => {
     try {
-      // Upload image to cloudinary
-      const result = await cloudinary.uploader.upload(req.file.path);
-
+     
       await Post.create({
         title: req.body.title,
-        image: result.secure_url,
-        cloudinaryId: result.public_id,
         caption: req.body.caption,
-        likes: 0,
-        user: req.user.id,
-        comment: req.body.comment
+        user: req.user.id
       });
-      console.log("Post has been added!");
+      console.log("Order has been added!");
       res.redirect("/profile");
     } catch (err) {
       console.log(err);
--- models/Post.js
+++ models/Post.js
@@ -5,30 +5,15 @@ const PostSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  image: {
-    type: String,
-    require: true,
-  },
-  cloudinaryId: {
-    type: String,
-    require: true,
-  },
+  
   caption: {
     type: String,
-    required: true,
-  },
-  likes: {
-    type: Number,
     required: true,
   },
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  }
 });
 
 module.exports = mongoose.model("Post", PostSchema);
